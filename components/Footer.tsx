23cc73f98276c30092c5a789036748060d0c6d21
Usman <tranktp100@gmail.com>
2024-08-25 00:18:36 +0100

    finish footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa6'
+import { socialMedia } from '@/data'
 
 const Footer = () => {
   return (
@@ -16,8 +19,29 @@ const Footer = () => {
           </h1>
         <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and lets discuss how i can help you achieve your goals</p>
           <a href="mailto:oshodiusman@yahoo.com">
-            <MagicButton/>
+            <MagicButton
+            title='Lets get in touch'
+            icon={<FaLocationArrow/>}
+            position='right'
+            />
           </a>
+        </div>
+        <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
+          <p className='md:text-base text-sm md:font-normal font-light '>Copyright © 2024 Usman </p>
+       
+        <div className='flex items-center md:gap-3 gap-6'>
+          {socialMedia.map((profile) => (
+               <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center
+                backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border
+                 border-black-300"
+               >
+                <img src={profile.img} alt={profile.img} width={20} height={20}/>
+
+               </div>
+
+
+          ))}
+        </div>
         </div>
 
 
