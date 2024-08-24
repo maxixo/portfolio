e96c2503e7d642c45cc1410495903b8b2844a19c
Usman <tranktp100@gmail.com>
2024-08-24 23:30:44 +0100

    started footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -17,6 +20,9 @@ export default function Home() {
       <Hero/>
       <Grid/>
       <RecentProjects/>
+      <Clients/>
+      <Experience/>
+      <Footer/>
     </div>
    </main>
   );
